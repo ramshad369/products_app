f273f5019888d1953660135939b4c444aea2d663
ramshad <ramshadak369@gmail.com>
2021-04-26 17:06:57 +0530

    mongo error

--- src/db/mongo1.js
+++ src/db/mongo1.js
@@ -1,8 +1,15 @@
 var mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_CONNECT,
-{ useNewUrlParser : true, useCreateIndex : true,useFindAndModify: false,
-    useUnifiedTopology: true},()=>
-{
-    console.log("mongo connected");
-})
+mongoose.connect('mongodb+srv://*********@********.mongodb.net/product-app-api? 
+         retryWrites=true', {useNewUrlParser: true});
+         mongoose.connection.once('open', function(){
+         console.log('Conection has been made!');
+             }).on('error', function(error){
+          console.log('Error is: ', error);
+           });
+// mongoose.connect(process.env.MONGODB_CONNECT,
+// { useNewUrlParser : true, useCreateIndex : true,useFindAndModify: false,
+//     useUnifiedTopology: true},()=>
+// {
+//     console.log("mongo connected");
+// })
