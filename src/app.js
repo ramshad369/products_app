47d93c71145b33a6bd8736fd5a6138d1116404de
ramshad <ramshadak369@gmail.com>
2021-04-26 16:05:02 +0530

    cors edit

--- src/app.js
+++ src/app.js
@@ -4,20 +4,22 @@ require('./db/mongo1')
 productRouter = require('./routers/products')
 
 const app = express()
-// const corsOpts = {
-//     origin: '*',
+const corsOpts = {
+    origin: '*',
   
-//     methods: [
-//       'GET',
-//       'POST',
-//     ],
+    methods: [
+      'GET',
+      'POST',
+      'PATCH',
+      'DELETE'
+    ],
   
-//     allowedHeaders: [
-//       'Content-Type',
-//     ],
-//   };
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
   
-// app.use(cors(corsOpts));
+app.use(cors(corsOpts));
 app.use(express.json())
 app.use(productRouter)
 
