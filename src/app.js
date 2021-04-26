e6a6402cc8c065629e84bb98161349d7fb3b1c38
ramshad <ramshadak369@gmail.com>
2021-04-26 10:11:16 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 require('./db/mongo1')
 productRouter = require('./routers/products')
 
 const app = express()
- 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json())
 app.use(productRouter)
 
