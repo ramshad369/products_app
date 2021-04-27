6bdb852ccf9a788bcaba0951b8768d4e7f4cf139
ramshad <ramshadak369@gmail.com>
2021-04-27 09:51:18 +0530

    mongo error url1

--- src/db/mongo1.js
+++ src/db/mongo1.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose')
 
+// mongoose.connect(process.env.MONGODB_URL,
+// {useNewUrlParser: true,useCreateIndex : true,useFindAndModify: false,
+//         useUnifiedTopology: true},);
+//          mongoose.connection.once('open', function(){
+//          console.log('Conection has been made!');
+//              }).on('error', function(error){
+//           console.log('Error is: ', error);
+        //    });
 mongoose.connect(process.env.MONGODB_URL,
-{useNewUrlParser: true,useCreateIndex : true,useFindAndModify: false,
-        useUnifiedTopology: true},);
-         mongoose.connection.once('open', function(){
-         console.log('Conection has been made!');
-             }).on('error', function(error){
-          console.log('Error is: ', error);
-           });
-// mongoose.connect(process.env.MONGODB_CONNECT,
-// { useNewUrlParser : true, useCreateIndex : true,useFindAndModify: false,
-//     useUnifiedTopology: true},()=>
-// {
-//     console.log("mongo connected");
-// }
+{ useNewUrlParser : true, useCreateIndex : true,useFindAndModify: false,
+    useUnifiedTopology: true},()=>
+{
+    console.log("mongo connected");
+})
